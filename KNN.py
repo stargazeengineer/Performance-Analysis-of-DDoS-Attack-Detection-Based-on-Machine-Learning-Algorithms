f019f97d5f33e9deeff917c7434a56d469a14bdc
stargazeengineer <160002543+stargazeengineer@users.noreply.github.com>
2025-02-06 18:17:51 +0800

    KNN.py

--- KNN.py
+++ KNN.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
